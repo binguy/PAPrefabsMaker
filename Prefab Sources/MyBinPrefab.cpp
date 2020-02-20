d796cf9c2277dbb7fa43927ae7307d494528ab04
MyBinGamer <Myself.Supr11@gmail.com>
2020-02-20 22:09:44 +0200

    Please Work

---
+++ Prefab Sources/MyBinPrefab.cpp
@@ -0,0 +1,11 @@
+/*#include "prefab.h"
+#include <math.h>
+
+Object *obj = new Object(0., 5, shapes::hx, 0, false, true, false);
+Object* parent1 = new Object(0., 5, shapes::cr, 0, false, true, true);
+
+int main(){
+prefab::name = "MyBinGamer";
+prefabTypes::Bomb;
+prefab::offset = 0.;
+}*/
